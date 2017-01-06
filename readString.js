d7c20d3939f31258e627daab6ded38b6b5dd5489
ioncrash <ioncrash@gmail.com>
2017-01-06 01:44:07 -0500

    readString removes punctuation, uppercases, splits
    does not handle emails?

--- readString.js
+++ readString.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const readString = (text) => {
-  console.log(text);
+  // let words = text.replace(/[,!?]/g,"");
+  let words = text.replace(/[,.!?]/g, '').toUpperCase().split(' ');
+  // if (words.includes('Pillyfish'))
+  return words;
 };
 
 module.exports = readString;
