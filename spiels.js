9ce99d1730b24d2d60217b51033c1097ad6be125
ioncrash <ioncrash@gmail.com>
2017-01-30 17:06:37 -0500

    add other responses

--- spiels.js
+++ spiels.js
@@ -1,6 +1,9 @@
 'use strict'
 
 module.exports = {
-  'TIC-TAC-TOE': "Tic-tac-toe was Ben's first project (don't judge him too hard!). https://goo.gl/fZSU3h",
-  'PILLYFISH': "Pillyfish is an app that keeps track of meds. It was designed for Ben's gothy gf, so it's purple. https://goo.gl/HO0Z9C",
+  'TIC-TAC-TOE': "Tic-tac-toe was Ben's first project (don't judge him too hard!) https://goo.gl/fZSU3h",
+  'PILLYFISH': "Pillyfish is an app that keeps track of meds. It was designed for Ben's gothy gf, so it's purple https://goo.gl/HO0Z9C",
+  'BUCKETLIST': "Bucketlist is an app using Google Maps API. It keeps track of your goals and displays their locations https://goo.gl/2gBmsw",
+  'PORTFOLIO': "This is Ben's portfolio! It uses a bootstrap template and has a pic of him DJing https://goo.gl/wgRAcs",
+  'BENBOTBZZZ': "That's me! I'm just a node script using Twitter's API! Check out my code over here: https://goo.gl/o1EQWM"
 };
